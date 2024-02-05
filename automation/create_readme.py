c9880327a208ad533902b3f23813389626b0412e
Myyel Isaac <147030259+JaquizeIsaac@users.noreply.github.com>
2024-02-05 15:03:21 -0800

    created readme

---
+++ automation/create_readme.py
@@ -0,0 +1,37 @@
+def create_readme():
+    readme_template = """
+# LAB - Class xx
+## Project: Project Name Here
+### Author: Student/Group Name
+
+### Links and Resources
+- [Back-end Server URL] # Fill in when applicable
+- [Front-end Application URL] # Fill in when applicable
+
+### Setup
+- Create a `.env` file with the following requirements:
+  - PORT - Port Number
+  - DATABASE_URL - URL to the running Postgres instance/db
+
+### How to Initialize/Run Your Application
+- Example: `python main.py`
+
+### How to Use Your Library
+- Provide instructions here if applicable
+
+### Tests
+#### How to Run Tests
+- Describe how to run tests
+
+#### Tests of Note
+- Any noteworthy information about the tests
+
+#### Incomplete Tests
+- Describe any tests that were not completed or skipped
+"""
+
+    with open('README.md', 'w') as readme:
+        readme.write(readme_template)
+
+# Call the function to create the README
+create_readme()
