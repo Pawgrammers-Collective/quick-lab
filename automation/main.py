fd6e0baff1d98c25c9eb5d522f1335d95cd8316a
kaleman18 <kylealeman18@gmail.com>
2024-02-06 11:02:41 -0800

    made a new file for user prompts and removed the prompts from main.py

--- automation/main.py
+++ automation/main.py
@@ -2,6 +2,7 @@ from automation.create_local_repo import init_local_repo
 from automation.create_gitignore import create_gitignore
 from automation.virtual_env import virtual_env_setup
 from automation.create_github_repo import create_github_repo
+from automation.user_prompts import user_prompts
 import os
 from rich.console import Console
 from automation.create_readme import create_readme
@@ -10,29 +11,13 @@ import subprocess
 
 def main():
     
-    #Finds the users current directory parent directory
-    current_directory=os.path.abspath(f'../{os.curdir}')
-
-    # User prompts
-    username = input("Enter your GitHub username: ")
-    repo_name = input("Enter the repository name: ")
-
-
-    # joins the directory and the repo name
-    directory=os.path.join(f'{current_directory}/', f'{repo_name}')
-    
     # Function calls
+    current_directory, directory, username, repo_name, pip_installs = user_prompts()
     init_local_repo(current_directory, repo_name)
     create_readme(directory)
     virtual_env_setup(directory)
     create_gitignore(directory)
-
-    # Adds and Commits the local repo created
-    subprocess.run(['git','add',"."], cwd=f"{directory}")
-    subprocess.run(['git','commit','-m','"First Commit"'], cwd=f"{directory}")
-
-    # links local repo and github repo
-    create_github_repo(repo_name, username,directory)
+    create_github_repo(repo_name, username, directory)
 
 
 
---
+++ automation/user_prompts.py
@@ -0,0 +1,44 @@
+import os
+
+
+def user_prompts():
+
+    # list for the users pip installs
+    pip_installs = []
+    pip_questions = True
+    login_question = True
+
+
+    # Checks if the user is logged in
+    while login_question:
+        question = input('For this to work properly you need to be logged into github on your browser.\nAre you logged in?\n(y/n):')
+
+        if question == "y":
+            login_question = False
+
+
+    # User prompts
+    username = input("Enter your GitHub username: ")
+    repo_name = input("Enter the repository name: ")
+    while pip_questions:
+
+        pip = input("Enter a dependency that you want to install: ")
+        pip_installs.append(pip)
+
+        exit_question = str(input("Do you want to add more dependencies(y/n): "))
+
+        print(exit_question)
+
+        if exit_question == "n":
+            pip_questions = False
+
+
+    #Finds the users current directory parents directory
+    current_directory=os.path.abspath(f'../{os.curdir}')
+
+
+    # joins the directory and the repo name
+    directory=os.path.join(f'{current_directory}/', f'{repo_name}')
+
+
+    return current_directory, directory, username, repo_name, pip_installs
