0440db640fd81db2ab91260338654759cd1f8c8c
JohnnyBackus <john.p.backus@gmail.com>
2024-02-06 16:21:06 -0800

    title and questions scraper working

--- automation/readings_scraper/readings_scraper.py
+++ automation/readings_scraper/readings_scraper.py
@@ -1,29 +1,59 @@
-from playwright.sync_api import sync_playwright
-from scrape_elements import get_title
+# from playwright.sync_api import sync_playwright
+from scrape_elements import get_title, get_questions
+import os
+# from dotenv import load_dotenv
+# load_dotenv()
 
 #TODO: find a way to pass username and password into the script
-#TODO: ask JB for dummy account
+
+# username = "john.p.backus@gmail.com"
+# password = "SEPtRh8^uY%DL#"
+# username = os.getenv('CANVAS_USERNAME')
+# password = os.getenv('CANVAS_PASSWORD')
 
 class_num = "28"
-url = "https://canvas.instructure.com/courses/8309911/discussion_topics/"
+# url = "https://canvas.instructure.com/courses/8309911/discussion_topics/"
+url = f"https://codefellows.github.io/code-401-python-guide/curriculum/class-{class_num}/DISCUSSION"
 
-def create_reading_assignment(class_num):
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False, slow_mo=1000)
-        page = browser.new_page()
-        page.goto(url)
-        page.click(f"text=Read: Class {class_num}")
-        print("clicked on Class")
-        title = get_title(page)
-        print("title: ", title)
+
+def create_reading_assignment():
+    title = get_title(url)
+    print("title: ", title)
+    questions = get_questions(url)
+    print("questions: ", questions)
+   
+    folder_path = "../reading_assignments"
+    file_name = f"reading{class_num}.md"
+    file_path = os.path.join(folder_path,"/",file_name)
+
+    # Create the folder if it doesn't exist
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
+    # Create the file
+    with open(file_path, "w") as file:
+        file.write(f"# {title}\n\n {questions}")
+
+
+# def create_reading_assignment(class_num):
+#     with sync_playwright() as p:
+#         browser = p.chromium.launch(headless=False, slow_mo=1000)
+#         page = browser.new_page()
+#         page.goto(url)
+#         page.fill('input[name="pseudonym_session[unique_id]"]', username)
+#         page.fill('input[name="pseudonym_session[password]"]', password)
+#         page.click('input[type="submit"]')
+#         page.click(f"text=Read: Class {class_num}")
+#         print("clicked on Class")
+#         title = get_title(page)
+#         print("title: ", title)
         # questions = get_questions(url)
         # print("questions: ", questions)
 
 
 
-# # with open("reading{class#}.md","w") as f:
-# #     f.write("")
+
 
 
 if __name__ == "__main__":
-    create_reading_assignment(class_num)
+    create_reading_assignment()
--- automation/readings_scraper/scrape_elements.py
+++ automation/readings_scraper/scrape_elements.py
@@ -2,17 +2,17 @@ from bs4 import BeautifulSoup
 import requests
 
 
-def get_title(page): #shared function to find assignment title
-    # response = requests.get(page)
-    text = page.content()
+def get_title(url): #shared function to find assignment title
+    response = requests.get(url)
+    # print("response: ", response)
+    text = response.text
     # print("text: ", text)
     soup = BeautifulSoup(text, "html.parser")
     # print("soup: ", soup)
-    title_soup = soup.find("div", class_="wrapper")
-    print("title_soup: ", title_soup)
-    # title_soup = soup.find("div", class_="wrapper").find("section").find("h1") #finds <h1> tag that is the 1st child of the 1st child section of div with the class "wrapper"
+    title_soup = soup.find("h1")
+    # print("title_soup: ", title_soup)
     title = title_soup.text
-    print("title: ", title)
+    # print("title: ", title)
     return title #returns a list
 
 
@@ -20,10 +20,10 @@ def get_questions(url): #shared function to find assignment questions
     response = requests.get(url)
     text = response.text
     soup = BeautifulSoup(text, "html.parser")
-    questions_soup = soup.find("div", class_="wrapper").find("section").find("h2", id="reading-questions").find_next_sibling("ol").findAll("li") #finds all <li> tags that are the children of the 1st child section of div with the class "wrapper"
-    print("questions_soup: ", questions_soup)
-    questions = questions_soup.text
-    print("questions: ", questions)
+    questions_soup = soup.find("h2", id="reading-questions").find_next_sibling("ol").findAll("li")#finds all <li> tags that are the children of the 1st child section of div with the class "wrapper"
+    # print("questions_soup: ", questions_soup)
+    questions = [q.text for q in questions_soup]
+    # print("questions: ", questions)
     return questions #returns a list
 
 
