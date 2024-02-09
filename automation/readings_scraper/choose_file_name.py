3d569239e0d52a004c368fec2f4cee2dfccd537b
JohnnyBackus <john.p.backus@gmail.com>
2024-02-09 10:15:06 -0800

    fixed bug

--- automation/readings_scraper/choose_file_name.py
+++ automation/readings_scraper/choose_file_name.py
@@ -26,18 +26,10 @@ def choose_file_name(class_num):
         file_name = input("Your file name must end in '.md'. Please enter your file name: ")
         if not file_name.endswith(".md"):
             console.print("Oops! Invalid file name. Your file name must end with '.md'. Returning to the file name prompt.", style="bold red")
+            file_name = f"reading{class_num}.md"
             choose_file_name(class_num)
         else:
-            console.print(f"You have chosen file name '{file_name}'. Please enter 'y' to confirm this is the file name you would like to use. Enter 'n' to change it (y/n):", style="bold green")
-            confirm_choice = input("> ")
-            if confirm_choice.lower() in ["y", "yes"]:
-                console.print(f"Ok! Your file name will be '{file_name}'.", style="bold green")
-            elif confirm_choice.lower() in ["n", "no"]:
-                console.print("Ok! Returning to the file name prompt.", style="bold yellow")
-                choose_file_name(class_num)
-            else:
-                console.print("Response not recognized. Returning to the file name prompt.", style="bold red")
-                choose_file_name(class_num)
+            console.print(f"Ok! Your file name is '{file_name}'.")
 
     else:
         console.print("Response not recognized. Returning to the file name prompt.", style="bold red")
